81785a3a3fa647989c5919848d491c584ba22f8f
Ian Ter Haar <ian.terhaar1004@gmail.com>
2025-11-30 15:41:04 +0200

    Create template endpoints

---
+++ TAE.UploadService/Controllers/UploadController.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TAE.UploadService.Controllers
+{
+    [ApiController]
+    [Route("api/v1/fileupload")]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost]
+        [Route("upload")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(Nullable), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<string>> Upload()
+        {
+            try
+            {
+                // logic
+                return "";
+            }
+            catch (Exception ex)
+            {
+                return (ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("upload/{fileId}/status")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(Nullable), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<string>> Status(int fileId)
+        {
+            try
+            {
+                // logic
+                return "";
+            }
+            catch (Exception ex)
+            {
+                return (ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("health")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(Nullable), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<string>> Health()
+        {
+            try
+            {
+                // logic
+                return "";
+            }
+            catch (Exception ex)
+            {
+                return (ex.Message);
+            }
+        }
+    }
+}
